c9cbd843d732e49654a2e9b775647e4002b30868
Tablesaw <tablesaw@gmail.com>
2018-12-16 21:38:51 -0800

    day 11b

--- stars/11b.py
+++ stars/11b.py
@@ -1,3 +1,6 @@
+import datetime
+import itertools
+
 SERIAL_NUMBER = 2866
 
 def power_level(x, y):
@@ -10,11 +13,39 @@ def power_level(x, y):
 
 grid = {(x, y): power_level(x, y) for x in range(1, 301) for y in range(1, 301)}
 
-def nine_total(x, y):
-    return sum(grid[(x+i, y+j)] for i in range(3) for j in range(3))
+# for x in range(1,11): 
+#     print(' '.join(str(grid[(x,y)]) for y in range(1,11)))
+# def nine_total(x, y):
+#     return sum(grid[(x+i, y+j)] for i in range(3) for j in range(3))
+# square_size = 0
 
-top = max( ( (x,y) for x in range(1,299) for y in range(1,299) ),
-    key=lambda c: nine_total(*c)
-    )
+# def square_total(x, y, s):
+#     return sum(grid[(x+i, y+j)] for i in range(s) for j in range(s))
 
-print(top)
+# top = max(
+#     ((x,y,s) for s in range(1, 301) for x in range(1,302-s) for y in range(1,302-s)),
+#     key=lambda c: square_total(*c)
+#     )
+
+def squares(grid):
+    for x in range(1, 301):
+        start = datetime.datetime.now()
+        for y in range(1, 301):
+            square_total = 0
+            max_square = 301 - max(x, y)
+            for s in range(1,max_square+1):
+                next_col = sum(grid[(x+s-1, y+row)] for row in range(s))
+                next_row = sum(grid[(x+col, y+s-1)] for col in range(s-1))
+                square_total = square_total + next_col + next_row
+                yield (x,y,s), square_total
+        elapsed = datetime.datetime.now() - start
+        print(x, elapsed)
+
+
+top_coord, val = max(squares(grid), key=lambda t: t[1])
+print(top_coord, val)
+# itr = squares(grid)
+# for n in range(10):
+#     coord, val = next(itr)
+#     check = square_total(*coord)
+#     print(coord, val, check)
